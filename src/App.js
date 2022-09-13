55f7e602b2e7d004621b9c8ae615c9d2f4c452a4
asrisitorus04 <asrisitorus04@gmail.com>
2022-09-13 22:16:16 +0700

    Update page login and register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css"
+import '../src/Style/Login.css';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import { Form, Button } from "react-bootstrap"
+import Header from "../component/Header"
+import { Link } from "react-router-dom"
+import '../Style/Login.css';
+
+const Login = () => {
+    return (
+        <div>
+            <Header />
+            <main className="form-container">
+                <Form className="form-box m-auto">
+                    <h1 className="login">Login</h1>
+                    <Form.Group className="title" controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control className="email" type="email" placeholder="Email" />
+                    </Form.Group>
+                    <Form.Group className="title" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control className="password" type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group className="button">
+                        <Button className="submit" type="submit">
+                            Sign In
+                        </Button>
+                    </Form.Group >
+                    <p className="signup">Dont have an account yet ? <Link to="/register">Sign Up</Link> here</p>
+                </Form>
+            </main>
+        </div>
+    );
+}
+
+export default Login
