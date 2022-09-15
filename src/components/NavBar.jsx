7e1a7d612a7d81c4809672acb5715fc7d42b34ff
Raka Fantino <rakafantino@gmail.com>
2022-09-16 01:00:19 +0700

    login with api provided by backend

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ function NavBar({ setToken }) {
           </Form>
           <Nav className="justify-end align-items-center gap-3">
             <NavLink to="/cart" className="position-relative">
-              <BsCartFill style={{ width: "2rem", height: "2rem" }} />
+              <BsCartFill style={{ width: "2rem", height: "2rem" }} className="text-dark" />
               {state.length === 0 ? <span></span> : <span class="position-absolute top-4 start-99 translate-middle badge rounded-pill bg-danger">{state.length}</span>}
             </NavLink>
             <NavLink to="/">
