aa3834bdb4b1f17bf1b5227b4f92cfa5e13369ad
Raka Fantino <rakafantino@gmail.com>
2022-09-16 01:48:37 +0700

    Fixing bugs everywhere cause of un-communicate merge to main barch

--- src/App.js
+++ src/App.js
@@ -13,9 +13,8 @@ import Register from "./pages/Register";
 import EditProfile from "./pages/EditProfile";
 import Profile from "./pages/Profile";
 import Checkout from "./pages/Checkout";
-import HistoryOrder from "./pages/HistoryOrder"
+import HistoryOrder from "./pages/HistoryOrder";
 import "./App.css";
-
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("userToken") ?? null);
@@ -24,20 +23,16 @@ function App() {
       <Router>
         {token ? <NavBar setToken={setToken} /> : null}
         <Routes>
-
-          <Route exact path="/" element={<Home />} />
-          <Route path="/detail/:id" element={<Detail />} />
-
           <Route exact path="/" element={token ? <Home /> : <Login token={token} setToken={setToken} />} />
           <Route path="/detail/:id" element={token ? <Detail /> : <Login token={token} setToken={setToken} />} />
           <Route path="/edit-product" element={token ? <EditProduct /> : <Login token={token} setToken={setToken} />} />
           <Route path="/cart" element={token ? <Cart /> : <Login token={token} setToken={setToken} />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/editprofile/:id" element={<EditProfile />} />
+          <Route path="/profile" element={token ? <Profile /> : <Login token={token} setToken={setToken} />} />
+          <Route path="/editprofile/:id" element={token ? <EditProfile /> : <Login token={token} setToken={setToken} />} />
+          <Route path="/checkout" element={token ? <Checkout /> : <Login token={token} setToken={setToken} />} />
+          <Route path="/historyorder" element={token ? <HistoryOrder /> : <Login token={token} setToken={setToken} />} />
           <Route path="*" element={<NotFound />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/historyorder" element={<HistoryOrder />} />
         </Routes>
         {token ? <Footer /> : null}
       </Router>
